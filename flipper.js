6f6ec22882430c0f34c1bdd548b7631295f901e7
zain <zainy9505@gmail.com>
2023-03-14 01:09:39 +0500

    Adding a Search box after Clicking on color code

--- flipper.js
+++ flipper.js
@@ -1,16 +1,23 @@
 $(function () {
-  $("#btn").click(function(){
+  $("#btn").click(function () {
     let hexColor = "#";
     const result = hexColor + Math.random().toString(16).substring(9);
-    $('#color').text(result)
-    $('body').css("background",result)
-})
-$('.border-btn').click(function(){
-  let hexColor = "#";
-  const result = hexColor + Math.random().toString(16).substring(9);
-  let border = "5px solid" + result;
-  $('header').css("border",border)
-})
-
+    $("#color").text(result);
+    $("body").css("background", result);
+  });
+  $(".border-btn").click(function () {
+    let hexColor = "#";
+    const result = hexColor + Math.random().toString(16).substring(9);
+    let border = "5px solid" + result;
+    $("header").css("border", border);
+  });
+  $("#color").click(function () {
+    let searchBar = $('<input type="text" id="input"/><button class="search"><i class="fa-solid fa-magnifying-glass"></i></button>');
+    let innerTxt = $("#color").html();
+    searchBar.attr("value", innerTxt);
+    $("#color").replaceWith(searchBar);
+    $('.search').click(function(){
+      $('body').css("background",$('#input').val())
+    })
+  });
 });
-
